248e9e506702ef7114f3ccaf2b3054cf28d679c0
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-21 16:56:21 +0330

    [fix] AuthForm
    Make LoginForm and Signup form for increase bugs.
    When we create login form and signup form with together, we got problem while register validation.
    So I ignore the forename and surname in mapLoop array but, the button submit not working and i think that because we dont declare or don't set value correct value for forename and surname fields.

--- src/components/auth/authForm.tsx
+++ src/components/auth/authForm.tsx
@@ -1,15 +1,9 @@
 "use client";
 
 import { useForm } from "react-hook-form";
-import { Fragment } from "react";
-import Link from "next/link";
 import { z } from "zod";
+import { loginValidation, signUpValidation } from "@/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  emailValidation,
-  forenameSurnameValidation,
-  passwordValidation,
-} from "@/validations";
 import {
   Form,
   FormControl,
@@ -18,63 +12,158 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
-import { Input } from "@/components/ui/input";
+import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { stringTitle } from "@/lib/utils";
+import { login } from "@/actions/auth";
+import SignUpPage from "@/app/(auth)/signup/page";
 
 interface AuthFormProps {
   mode?: "login" | "register";
 }
 
-const AuthForm: React.FC<AuthFormProps> = ({ mode = "login" }) => {
-  let zodFormSchema = emailValidation
-    .merge(passwordValidation)
-    .merge(forenameSurnameValidation);
-  const fields = ["forename", "surname", "email", "password"] as const;
-  const form = useForm<z.infer<typeof zodFormSchema>>({
-    resolver: zodResolver(zodFormSchema),
+const LoginForm: React.FC = () => {
+  const form = useForm<z.infer<typeof loginValidation>>({
+    resolver: zodResolver(loginValidation),
     defaultValues: {
       email: "",
       password: "",
-      forename: "",
-      surname: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof zodFormSchema>) {
-    console.log(values);
+  async function onSubmit({
+    email,
+    password,
+  }: z.infer<typeof loginValidation>) {
+    await login(email, password);
   }
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
-        {fields!.map((name) => {
-          // for ignore forename and surname input in login page
-          if (mode === "login" && ["forename", "surname"].includes(name)) {
-            return <Fragment key={name}></Fragment>;
-          }
-          return (
-            <FormField
-              control={form.control}
-              name={name}
-              key={name}
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>{stringTitle(name)}:</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-          );
-        })}
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+        method="POST"
+      >
+        <FormField
+          control={form.control}
+          name={"email"}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email:</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={"password"}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password:</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <Button type="submit">Login</Button>
       </form>
     </Form>
   );
+};
+
+const SignupForm: React.FC = () => {
+  const form = useForm<z.infer<typeof signUpValidation>>({
+    resolver: zodResolver(signUpValidation),
+    defaultValues: {
+      forename: "",
+      surname: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit({
+    forename,
+    surname,
+    email,
+    password,
+  }: z.infer<typeof signUpValidation>) {
+    await login(email, password);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+        method="POST"
+      >
+        <FormField
+          control={form.control}
+          name={"forename"}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Forename:</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={"surname"}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Surname:</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={"email"}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email:</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={"password"}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password:</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Login</Button>
+      </form>
+    </Form>
+  );
+};
+
+const AuthForm: React.FC<AuthFormProps> = ({ mode = "login" }) => {
+  if (mode === "login") return <LoginForm />;
+  return <SignupForm />;
 };
 
 export default AuthForm;
