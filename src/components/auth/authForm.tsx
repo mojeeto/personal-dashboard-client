e9988c1a6479cab048439e3fd9384c123e5a9588
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-21 16:00:15 +0330

    [feat] AuthForm Component

--- src/components/auth/authForm.tsx
+++ src/components/auth/authForm.tsx
@@ -1,19 +1,74 @@
+"use client";
+
+import { useForm } from "react-hook-form";
 import Link from "next/link";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  emailValidation,
+  forenameSurnameValidation,
+  passwordValidation,
+} from "@/validations";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { stringTitle } from "@/lib/utils";
 
 interface AuthFormProps {
   mode?: "login" | "register";
 }
 
 const AuthForm: React.FC<AuthFormProps> = ({ mode = "login" }) => {
+  let zodFormSchema = emailValidation
+    .merge(passwordValidation)
+    .merge(forenameSurnameValidation);
+  const fields = ["forename", "surname", "email", "password"] as const;
+  const form = useForm<z.infer<typeof zodFormSchema>>({
+    resolver: zodResolver(zodFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      forename: "",
+      surname: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof zodFormSchema>) {
+    console.log(values);
+  }
   return (
-    <form>
-      <input type="email" name="email" required />
-      <input type="password" name="password" required />
-      <button>{mode.toUpperCase()}</button>
-      <Link href={`/${mode}`}>
-        {mode === "login" ? "Create New Account" : "Login with existing Email"}
-      </Link>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        {fields!.map((name) => {
+          return (
+            <FormField
+              control={form.control}
+              name={name}
+              key={name}
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>{stringTitle(name)}:</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          );
+        })}
+        <Button type="submit">Login</Button>
+      </form>
+    </Form>
   );
 };
 
