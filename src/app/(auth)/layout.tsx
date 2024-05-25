0ec29bc4885e9e1c6db2b982380a8aa84be97612
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-25 11:30:01 +0330

    [test] test authentication in layout pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
-export default function RootLayout({
+import { redirect } from "next/navigation";
+import { isAuthenticate } from "@/lib/auth";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isAuth = await isAuthenticate();
+  if (isAuth && isAuth.jwt_token) {
+    redirect("/dashboard");
+  }
   return <>{children}</>;
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,11 @@
-export default function RootLayout({
+import { isAuthenticate } from "@/lib/auth";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isAuth = await isAuthenticate();
+  console.log(isAuth);
   return <>{children}</>;
 }
