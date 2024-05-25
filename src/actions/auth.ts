6f7b140e7fd98fe1ca7da4e2aa20252f0ea56ef4
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-25 11:28:47 +0330

    [feat] authenticate after login

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,7 +1,32 @@
 "use server";
 
+import { createAuthSession } from "@/lib/auth";
+import { createUser } from "@/repo/user";
+import { redirect } from "next/navigation";
+
+const API_URL = process.env.API_URL!;
+
 export async function loginAction(email: string, password: string) {
-  console.log(email, password);
+  const request = await fetch(API_URL + "login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+  const response = await request.json();
+  if (response.statusCode !== 200) {
+    return {
+      message: response.message,
+    };
+  }
+
+  const newUserId = createUser(email);
+
+  createAuthSession(newUserId.toString(), response.data.jwt_token);
 }
 
 export async function signupAction(
@@ -10,7 +35,7 @@ export async function signupAction(
   email: string,
   password: string,
 ) {
-  const request = await fetch("http://localhost:8080/signup", {
+  const request = await fetch(API_URL + "signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
