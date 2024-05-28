3ec624e38bda0b2216da8d525047d8b2400e72c8
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-28 15:37:55 +0330

    [dev] test wallet dialog

--- src/app/(dashboard)/wallet/page.tsx
+++ src/app/(dashboard)/wallet/page.tsx
@@ -1,15 +1,14 @@
 import PlusIcon from "@/components/icons/plusIcon";
 import { Button } from "@/components/ui/button";
-import WalletTable from "@/components/wallet";
+import WalletTable from "@/components/wallet/wallet-table";
+import WalletRecordDialog from "@/components/wallet/wallet-record-dialog";
 
 export default function WalletPage() {
   return (
     <main>
       <div className="flex items-center justify-between mb-5">
         <h1>Wallet Records</h1>
-        <Button size="icon" variant={"outline"}>
-          <PlusIcon />
-        </Button>
+        <WalletRecordDialog />
       </div>
       <WalletTable />
     </main>
